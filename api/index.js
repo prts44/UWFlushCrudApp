63cf85a0a226aecc1bad3a32127ecf4bac614ef9
z <ryan.ckit57@gmail.com>
2022-09-19 18:09:20 -0400

    Initial API

---
+++ api/index.js
@@ -0,0 +1,49 @@
+const mysql = require('mysql2');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cred = require('./credentials');
+const app = express();
+const port = 3001;
+var con = mysql.createConnection({
+    port: 3306,
+    host: 'localhost',
+    user: cred.user,
+    password: cred.pass,
+    database: cred.dbname
+});
+   
+con.connect();
+
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.get("/", (req, res) => {
+    res.send("hello");
+});
+
+app.get("/locations", (req, res) => {
+    con.promise().query('SELECT * FROM locations')
+        .then(([rows, fields]) => {
+            console.log(rows);
+            res.json(rows);
+        });
+});
+
+app.post("/locations/add", (req, res) => {
+    console.log(req.body);
+    con.promise().query(`INSERT INTO locations (name, building, admin_rating, gender) VALUES (${req.body.name}, ${req.body.building}, ${req.body.admin_rating}, ${req.body.gender})`);
+    res.status(201).json("done");
+});
+
+app.delete("/locations/delete/:id", (req, res) => {
+    console.log(req.params.id);
+    con.promise().query(`DELETE FROM locations WHERE id=${req.params.id}`);
+    res.status(201).json("done");
+});
+
+con.query('SELECT * FROM locations', (error, results, fields) => {
+    console.log(results);
+});
